2e2cb4ed2c275158062099165b23dabfbe7ff170
Philippe Deslauriers <philde@chainguard.dev>
2022-12-27 23:30:43 -0800

    v0.0.11, with scripted release

---
+++ packages/js-containerregistry/hack/release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Script for publishing a new release.
+#
+# Usage:
+#    Create a new release
+#      ./hack/publish.sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+pushd go
+GOOS=js GOARCH=wasm go build -o ../wasm/containerregistry.wasm ./cmd/wasm
+popd
+
+rm -rf dist
+pnpm dist
+
+PACK=$(npm pack)
+
+echo "Signing release..."
+LOG_INDEX=$(pnpm exec sigstore sign "${PACK}" | jq -r '.verificationData.tlogEntries[0].logIndex')
+echo "Rekor entry: https://rekor.tlog.dev/?logIndex=${LOG_INDEX}"
+
+echo "Publishing..."
+npm publish --access public ${PACK}
+
+echo "Cleaning up..."
+rm ${PACK}
+rm -rf dist
